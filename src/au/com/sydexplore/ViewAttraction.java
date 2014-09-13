e305be9697b575dffb095960f8ca97c3ba65aa20
jleu1656 <jessieleung@Dumpling.local>
2014-09-13 21:47:59 +1000

    Change activity xml

--- src/au/com/sydexplore/ViewAttraction.java
+++ src/au/com/sydexplore/ViewAttraction.java
@@ -14,7 +14,7 @@ public class ViewAttraction extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_attraction);
     }
     
     /** 
