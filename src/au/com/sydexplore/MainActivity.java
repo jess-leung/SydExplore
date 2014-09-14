1913c54439b748d46aed06342f7ef81e05f3de61
jleu1656 <jessieleung@Dumpling.local>
2014-09-14 18:25:58 +1000

    Adding in main activity category list view

--- src/au/com/sydexplore/MainActivity.java
+++ src/au/com/sydexplore/MainActivity.java
@@ -1,5 +1,7 @@
 package au.com.sydexplore;
 
+import java.util.ArrayList;
+
 import au.com.sydexplore.R;
 
 import android.app.Activity;
@@ -8,13 +10,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	
+	// List view for categories 
+	ListView categoriesList; 
+	// Array list containing the categories 
+	ArrayList<String> categoryArray; 
+	// ArrayAdapter 
+	ArrayAdapter<String> categoriesAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Set content view 
         setContentView(R.layout.activity_main);
+        
+        // reference the "listview" variable to the id-"listview" in the layout
+     	categoriesList = (ListView) findViewById(R.id.categoriesList);
+     		
+        // Construct the data source
+     	categoryArray = new ArrayList<String>();
+     	
+     	// Add in default categories 
+     	categoryArray.add("Adventurous");
+     	categoryArray.add("Social");
+     	categoryArray.add("Cultural"); 
+     	categoryArray.add("Historical"); 
+     	categoryArray.add("Education");
+     	categoryArray.add("Hungry"); 
+     	categoryArray.add("Natural");
+     	categoryArray.add("Lazy");
+     	categoryArray.add("Luxurious"); 
+     	
+     	// Initialize array adapter for categories 
+        categoriesAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,categoryArray);
+        
+        // Attach the adapter to a ListView
+        categoriesList.setAdapter(categoriesAdapter);
     }
     
     /** 
