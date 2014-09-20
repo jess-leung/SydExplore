c32313a1cfa31abddf0c2d966d3cde27654a6445
jleu1656 <jessieleung@Dumpling.local>
2014-09-20 14:57:50 +1000

    Returning JSON response

--- src/au/com/sydexplore/MainActivity.java
+++ src/au/com/sydexplore/MainActivity.java
@@ -1,17 +1,37 @@
 package au.com.sydexplore;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 
 import au.com.sydexplore.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 public class MainActivity extends Activity {
 	
@@ -21,6 +41,8 @@ public class MainActivity extends Activity {
 	ArrayList<String> categoryArray; 
 	// ArrayAdapter 
 	ArrayAdapter<String> categoriesAdapter;
+	static InputStream is = null;
+    static String jsonin = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +72,8 @@ public class MainActivity extends Activity {
         
         // Attach the adapter to a ListView
         categoriesList.setAdapter(categoriesAdapter);
+        
+        setupListViewListener(); 
     }
     
     /** 
@@ -61,4 +85,69 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(MainActivity.this, ViewAttraction.class);
         startActivity(intent);
     }
+    
+    
+	private void setupListViewListener() { 
+		categoriesList.setOnItemClickListener(new OnItemClickListener() { 
+			@Override
+			public void onItemClick(AdapterView <? > parent, View view, int position, long id) { 
+				String categoryClickedOn = categoriesAdapter.getItem(position); //.getText(); 
+				Log.i("MainActivity", "Clicked item " + position + ": " + categoryClickedOn); 
+				sendJson(categoryClickedOn);
+				
+			} 
+		}); 
+	}
+    
+    /** 
+     * Send JSON to backend 
+     * @param category
+     */
+	private void sendJson(final String category) {
+		Thread t = new Thread() {
+
+			public void run() {
+				HttpClient client = new DefaultHttpClient();
+				HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+				
+				//HttpResponse response;
+	            JSONObject json = new JSONObject();
+
+	            try {
+	            	HttpPost post = new HttpPost("http://lit-cove-9769.herokuapp.com/attractions/getAttractions/");
+	            	// construct JSON output 
+	            	json.put("category_name", category);
+	            	Log.d("JSON", json.toString());
+	            	StringEntity se = new StringEntity(json.toString());  
+	            	se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+	            	post.setEntity(se);
+	            	HttpResponse httpResponse = client.execute(post);
+	    			HttpEntity httpEntity = httpResponse.getEntity();
+	    			is = httpEntity.getContent();    
+	            	Log.d("JSON", "Got content from entity");
+        
+	            } catch(Exception e) {
+	            	e.printStackTrace();
+	            	Log.d("JSON", "Caught Exception");
+	            }
+	            
+	            try {
+	    			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+	    			StringBuilder sb = new StringBuilder();
+	    			String line = null;
+	    			while ((line = reader.readLine()) != null) {
+	    				sb.append(line + "\n");
+	    			}
+	    			is.close();
+	    			jsonin = sb.toString();
+	    			Log.d("JSON", jsonin);
+	    		} catch (Exception e) {
+	    			Log.d("JSON", "Caught Exception");
+	    			Log.e("Buffer Error", "Error converting result " + e.toString());
+	    		}
+	    		Log.d("JSON", "Json String returned");
+			}
+		};
+		t.start();      
+	}
 }
